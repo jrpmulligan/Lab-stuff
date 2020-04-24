1646ba3cc6465a3a0cceb5799533162c4a460aed
jrpmulligan <55244046+jrpmulligan@users.noreply.github.com>
2020-04-23 23:26:18 -0500

    gff_to_table program

---
+++ gff_to_table.py
@@ -0,0 +1,51 @@
+import sys
+import re
+
+f=open(sys.argv[1]) #open file and make into array
+farray=f.readlines()
+del farray[1],farray[0]
+f.close()
+
+def Gettheinfo(gffarray): #get the information needed for the table
+    theinfo=[]
+    for i in gffarray:
+        u=[]
+        if 'EoCCA6' in i:
+            z=i.split()
+            if 'CDS' in i: #helps make index numbers consistent
+                z.remove('CDS')
+            
+            if len(z)>=8:
+                gene=re.search('EoCCA6(.+?);',z[8])#getting the geneID
+                if gene !=None:
+                    u=[z[0],gene.group(0),z[3],z[4]]#information index numbers into new array
+                    if z[6]=='+':
+                        u.append('forward')
+                        u.append('1')
+                    else:
+                        u.append('reverse')
+                        u.append('-1')
+                    theinfo.append(u) #add array to matric
+                
+    return theinfo
+theinfo=(Gettheinfo(farray))
+
+orig_stdout=sys.stdout #print to file
+g=open(sys.argv[2],'w')
+sys.stdout=g
+print('molecule\tgene\tstart\tend\tstrand\tdirection')
+for h in theinfo:      
+    print(h[0]+'\t'+h[1]+'\t'+h[2]+'\t'+h[3]+'\t'+h[4]+'\t'+h[5])
+sys.stdout=orig_stdout
+g.close()
+    
+    
+
+
+        
+    
+"""
+
+
+"""
+
