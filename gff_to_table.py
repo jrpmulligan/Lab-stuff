8937951485d142141d87441ad15d42e98d4e5612
jrpmulligan <55244046+jrpmulligan@users.noreply.github.com>
2020-05-01 14:39:41 -0500

    hcp identify
    Added ability to identify gene=hcp entries and populated the gene column with 'hcp' for said entries.

--- gff_to_table.py
+++ gff_to_table.py
@@ -7,6 +7,8 @@ farray=f.readlines()
 del farray[1],farray[0]
 f.close()
 
+
+
 def Gettheinfo(gffarray): #get the information needed for the table
     theinfo=[]
     for i in gffarray:
@@ -15,8 +17,19 @@ def Gettheinfo(gffarray): #get the information needed for the table
         if 'CDS' in i: #helps make index numbers consistent
             z.remove('CDS')
         if len(z)>=8:
-            gene=re.search('ID=(.+?);',z[8])#getting the geneID
-            if gene !=None:
+            gene=re.search('ID=(.+?);',z[8])#getting the geneID 
+            #print(z)
+            gene2=re.search('gene=hcp;',z[8])
+            if gene2!=None:
+                u=[z[0],'hcp',z[3],z[4]]#information index numbers into new array
+                if z[6]=='+':
+                    u.append('forward')
+                    u.append('1')
+                else:
+                    u.append('reverse')
+                    u.append('-1')
+                theinfo.append(u) #add array to matric
+            elif gene !=None:
                 u=[z[0],gene.group(1),z[3],z[4]]#information index numbers into new array
                 if z[6]=='+':
                     u.append('forward')
@@ -25,26 +38,10 @@ def Gettheinfo(gffarray): #get the information needed for the table
                     u.append('reverse')
                     u.append('-1')
                 theinfo.append(u) #add array to matric
-                
     return theinfo
 theinfo=(Gettheinfo(farray))
 
-orig_stdout=sys.stdout #print to file
-g=open(sys.argv[2],'w')
-sys.stdout=g
 print('molecule\tgene\tstart\tend\tstrand\tdirection')
 for h in theinfo:      
     print(h[0]+'\t'+h[1]+'\t'+h[2]+'\t'+h[3]+'\t'+h[4]+'\t'+h[5])
-sys.stdout=orig_stdout
-g.close()
-    
-    
-
-
-        
-    
-"""
-
-
-"""
 
